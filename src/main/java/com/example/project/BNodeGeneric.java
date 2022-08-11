437ffe71e9a61993fa5a09d52a29e70a97e7a28a
HeidiCordova <88119136+HeidiCordova@users.noreply.github.com>
2022-08-11 15:31:13 -0500

    Create BNodeGeneric.java

---
+++ src/main/java/com/example/project/BNodeGeneric.java
@@ -0,0 +1,6 @@
+/*Implemente una clase BNodeGeneric<T> donde T es un tipo genérico, esta
+clase debe contener al menos las siguientes propiedades.
+- Vector<T> keys: la información almacenada en el nodo.
+- Vector<BNodeGeneric<T>> children: una referencia a los hijos del
+nodo.
+*/
